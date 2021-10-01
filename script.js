63e7f5fe09f5a3c029df77bcb41b9270f003f526
cathytanya <cathycolette.tanya@gmail.com>
2021-10-01 00:04:38 -0400

    set variables for the needed information from the api. confirmed that this api is correct

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+set variables to call information from the html
 let button = document.querySelector(".buttonSubmit")
 let inputCity = document.querySelector(".inputCity")
 let cityName = document.querySelector(".cityName")
 let temp = document.querySelector("temp")
-let info = document.querySelector("info")
+let humidity = document.querySelector("humidity")
+let windSpeed = document.querySelector("windSpeed")
+let indexUV = document.querySelector("indexUV")
+
 
 button.addEventListener("click",function(){
     //Aapi.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
@@ -10,6 +14,12 @@ button.addEventListener("click",function(){
     fetch("https://api.openweathermap.org/data/2.5/forecast?q="+inputCity.value+"&appid=d9e0fa417e5a25b1237d9e6b7fcee2e0")
     .then(response => response.json())
     .then(data => console.log(data))
+    .then( data => {
+        let city = data['cityName']
+        let tempVal = data["main"]["temp"]
+        let
+    })
+    cityName
 
     .catch (err => alert("Wrong city!"))
 })
